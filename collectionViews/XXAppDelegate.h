5ca461e55294c0cf8a3f5de7f60ecf53b938852b
shanxu <kingundertree@163.com>
2014-05-23 18:21:47 +0800

    add pro

---
+++ collectionViews/XXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXAppDelegate.h
+//  collectionViews
+//
+//  Created by xiazer on 14-5-23.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ collectionViews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  collectionViews
+//
+//  Created by xiazer on 14-5-23.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XXAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XXAppDelegate class]));
+    }
+}
